5b32b3d381ed9de474011f37ee2b2713e8da2a5c
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-09-14 18:57:10 -0500

    Fixed quiz question

--- client/src/components/Question/index.js
+++ client/src/components/Question/index.js
@@ -1,14 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { Container, Grid, Header, Radio, Rating, Button } from 'semantic-ui-react';
-import { useQuery } from '@apollo/client';
-import { QUERY_IMAGES } from '../../utils/queries';
+import React, { useEffect, useState } from "react";
+import {
+  Container,
+  Grid,
+  Header,
+  Radio,
+  Rating,
+  Button,
+} from "semantic-ui-react";
+import { useQuery } from "@apollo/client";
+import { QUERY_IMAGES } from "../../utils/queries";
+import { Routes, useNavigate, Navigate } from "react-router-dom";
 
-
-export 
-
-function Question() {
-
-
+export function Question() {
   const [activeQuestion, setActiveQuestion] = useState(0);
 
   const [isDisabled, setDisabled] = useState(false);
@@ -49,28 +52,26 @@ function Question() {
         navigateProfile();
       }
     };
-  
-      return (
-        <>
-        <Grid.Row>
-          <div>Rating: {rating}</div>
-          <input
-            type='range'
-            min={1}
-            max={5}
-            value={rating}
-            onChange={handleChange}
-          /><br />
-          <Rating
-            icon="star"
-            maxRating={5}
-            rating={rating}
-          />
-        </Grid.Row>
-        <Grid.Row>
-        <Button primary onClick={onClickNext}>
-            Next
 
+    return (
+      <>
+        <div>
+          <Grid.Row>
+            <div>Rating: {rating}</div>
+            <input
+              type="range"
+              min={1}
+              max={5}
+              value={rating}
+              onChange={handleChange}
+            />
+            <br />
+            <Rating icon="star" maxRating={5} rating={rating} />
+          </Grid.Row>
+        </div>
+        <Grid.Row>
+          <Button primary onClick={onClickNext}>
+            {isDisabled ? "See Your Results" : "Next"}
           </Button>
         </Grid.Row>
       </>
@@ -87,23 +88,17 @@ function Question() {
             <p> Rate the following Artworks</p>
           </Header>
 
-      <Header as='h3' textAlign='center'>
-       <p> Rate the following Artworks</p>
-      </Header>
-
-         <Grid textAlign='center'>
-       
-        <Grid.Row> <img src={currentImage.src} alt={currentImage.src} height = '200' width = '200' margin = '10px' padding = '10px'></img>
-         </Grid.Row>
-                 <Grid.Row>
-                    <Grid.Column>
-                      <RatingSlider />
-                    </Grid.Column>
-                  </Grid.Row>
-                 
-                  <Grid.Row key='temp'>
-            <div>{showResults()}</div>
-
+          <Grid textAlign="center">
+            <Grid.Row>
+              {" "}
+              <img
+                src={currentImage.src}
+                alt={currentImage.src}
+                height="200"
+                width="200"
+                margin="10px"
+                padding="10px"
+              ></img>
             </Grid.Row>
 
             <RatingSlider />
