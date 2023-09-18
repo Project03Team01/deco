153e6a7d6072d7e315f7de09dd86517e0ae75904
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-09-18 12:23:36 -0500

    Added UserProfile img array, but fetch not working

--- client/src/components/UserProfile/index.js
+++ client/src/components/UserProfile/index.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
-import { QUERY_USER, QUERY_IMAGES } from "../../utils/queries";
+import { QUERY_USER, QUERY_CATEGORIES, QUERY_CATEGORY_IMAGES } from "../../utils/queries";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
 import ImageCarousel from "../ImageCarousel";
@@ -8,60 +8,89 @@ import { List, Container, Segment, Header, Button } from "semantic-ui-react";
 import Jumbotron from "../Jumbotron";
 
 function UserProfile() {
-
   const {loading: loadingUser, data: userData } = useQuery(QUERY_USER);
   const user = userData?.user || [];
 
-  const {loading: loadingImages, data: imagesData} = useQuery(QUERY_IMAGES);
-  const images = imagesData?.images || [];
+  const { loading: categoryLoading, data: categoryData } = useQuery(QUERY_CATEGORIES);
 
-  let category1;
-  let category2;
-  let category3;
-  let categoryNameArray = [];
-  let categoryIdArray = [];
-  let image1 = "";
-  let image2 = "";
-  let image3 = "";
-  let imageSrcArray = [];
+  const categoryMappings = {};
 
-  function pullCategoryData() {
-  
-    const topCategoriesObj = user.userScore;
+  console.log(categoryData)
 
-    let sortable = [];
-    for( var category in topCategoriesObj){
-      sortable.push([category, topCategoriesObj[category]]);
-    };
-    sortable.shift();
-    const topCategoriesSorted = sortable.sort(function(a,b){
-      return b[1]-a[1]
+  if (categoryData) {
+    categoryData.categories.forEach(category => {
+      categoryMappings[category.scoreCategory] = category._id;
     });
-
-    category1 = topCategoriesSorted[0];
-    category2 = topCategoriesSorted[1];
-    category3 = topCategoriesSorted[2];
-
-    for (let i = 0; i < images.length; i++) {
-      let imageCategory = images[i].category;
-      if (imageCategory.scoreCategory === category1[0]){
-        category1 = imageCategory;
-        image1 = images[i];
-      } else if (imageCategory.scoreCategory === category2[0]) {
-        category2 = imageCategory;
-        image2 = images[i];
-      } else if (imageCategory.scoreCategory === category3[0]) {
-        category3 = imageCategory;
-        image3 = images[i];
-      } 
-    }
-    categoryNameArray = [category1.name, category2.name, category3.name];
-    categoryIdArray = [category1._id, category2._id, category3._id];
-    imageSrcArray = [image1, image2, image3];
   }
   
+  const [combinedImages, setCombinedImages] = useState([]);
+  const [categoryIds, setCategoryIds] = useState([]);
+  const [categoryNames, setCategoryNames] = useState([]);
 
-  
+  useEffect(() => {
+    const fetchData = async () => {
+      const topCategoriesObj = user.userScore;
+      console.log(topCategoriesObj);
+      let sortable = [];
+      for (var category in topCategoriesObj) {
+        sortable.push([category, topCategoriesObj[category]]);
+      }
+      sortable.shift();
+      const topCategoriesSorted = sortable.sort(function (a, b) {
+        return b[1] - a[1];
+      });
+
+      const ids = topCategoriesSorted.slice(0, 3).map((entry) => entry[0]);
+      setCategoryIds(ids);
+
+      const names = topCategoriesSorted.slice(0, 3).map((entry) => entry[1]);
+      setCategoryNames(names);
+
+      const categoryImages = await Promise.all(
+        ids.map(async (scoreCategory) => {
+          
+          const categoryId = categoryMappings[scoreCategory];
+
+          console.log(categoryId);
+          try {
+            const response = await fetch(`/art/${categoryId}`, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                query: `
+                  query {
+                    categoryImages(categoryId: "${categoryId}") {
+                      _id
+                      src
+                      artist
+                      category {
+                        name
+                        description
+                      }
+                    }
+                  }
+                `,
+              }),
+            });
+
+            const data = await response.json();
+            return data.data.categoryImages;
+          } catch (error) {
+            console.error("Error fetching category images:", error);
+            return [];
+          }
+        })
+      );
+      const combined = categoryImages.flat();
+      setCombinedImages(combined);
+    };
+
+    if (!loadingUser) {
+      fetchData();
+    }
+  }, [loadingUser, user]);
 
   const handleLogout = async (event) => {
     event.preventDefault();
@@ -72,48 +101,32 @@ function UserProfile() {
     }
   };
 
-  if(!loadingUser && !loadingImages){
+  if(!loadingUser){
     if (Auth.loggedIn()){
-      pullCategoryData();
       return (
         <Container >
           <Segment.Group horizontal>
-              <Segment horizontal textAlign="center">
-                <h1 font-size="50px">{user.firstName} {user.lastName}</h1>
-                <Segment textAlign="center" s>
+            <Segment horizontal textAlign="center">
+              <h1 font-size="50px">{user.firstName} {user.lastName}</h1>
+              <Segment textAlign="center">
                 <Button onClick={handleLogout}>Log Out</Button>
               </Segment>
-              </Segment>
+            </Segment>
               
             <Segment textAlign="center">
               <Header>Top Art Styles</Header>
               <List>
-                <List.Item>
-                  <Link 
-                    to={`/art/${categoryIdArray[0]}`} 
-                    style={{color: "black", fontWeight: "bold"}}
-                  >
-                    {categoryNameArray[0]}
-                  </Link>
-                </List.Item>
-                <List.Item>
-                  <Link 
-                    to={`/art/${categoryIdArray[1]}`} 
-                    style={{color: "black", fontWeight: "bold"}}
-                  >
-                    {categoryNameArray[1]}
-                  </Link>
-                </List.Item>
-                <List.Item>
-                  <Link 
-                    to={`/art/${categoryIdArray[2]}`} 
-                    style={{color: "black", fontWeight: "bold"}}
-                  >
-                    {categoryNameArray[2]}
-                  </Link>
-                </List.Item>
+                {combinedImages.slice(0, 3).map((image, index) => (
+                  <List.Item key={index}>
+                    <Link
+                      to={`/art/${categoryIds[index]}`}
+                      style={{ color: "black", fontWeight: "bold" }}
+                    >
+                      {categoryNames[index]}
+                    </Link>
+                  </List.Item>
+                ))}
               </List>
-              
             </Segment>
           </Segment.Group>
           <Segment.Group >
@@ -121,41 +134,35 @@ function UserProfile() {
               <Header>Top Art Pieces</Header>
             </Segment>
             <Segment> 
-              <ImageCarousel images={imageSrcArray} />
+              <ImageCarousel images={combinedImages.map((image) => image.src)} />
             </Segment>
           </Segment.Group>
-          
-
         </Container>
       );
     } else {
-      return(
-      <Jumbotron>
-        <h1>You Are Not Logged In!</h1>
-        <Button
-          className="primary"
-          name="login"
-          as={Link}
-          to="/login"
-        >
-          Login Here
-        </Button>
-        <Button
-          className="secondary"
-          name="signup"
-          as={Link}
-          to="/signup"
-        >
-          Signup
-        </Button>
-
-        
-      </Jumbotron>
+      return (
+        <Jumbotron>
+          <h1>You Are Not Logged In!</h1>
+          <Button
+            className="primary"
+            name="login"
+            as={Link}
+            to="/login"
+          >
+            Login Here
+          </Button>
+          <Button
+            className="secondary"
+            name="signup"
+            as={Link}
+            to="/signup"
+          >
+            Signup
+          </Button>
+        </Jumbotron>
       );
     }
   }
-
-
 }
-  
+
 export default UserProfile;
